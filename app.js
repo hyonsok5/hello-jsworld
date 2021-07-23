b73f4c1362fa811d024db779e85278fef7212739
hyonsok <s@hyonsok.io>
2021-07-23 13:01:49 +0900

    temporary saved.

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 const container = document.getElementById('root');
 
 const ajax = new XMLHttpRequest();
-const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
   
@@ -14,42 +13,34 @@ function getData(url){
 }  
   
 const newsFeed = getData(NEWS_URL);
-const ul = document.createElement('ul');
  
 window.addEventListener('hashchange', function(){
 
   const id = location.hash.substr(1);
-
   const newsContent = getData(CONTENT_URL.replace('@id',id));
-   
-  const title = this.document.createElement('h1');
 
-  title.innerHTML = newsContent.title;
-  content.appendChild(title);
+  container.innerHTML = `
+    <h1>${newsContent.title}</h1>
 
+    <div>
+      <a href='#'>to List Page</a>
+    </div>
+  `; 
 
 });
 
+
+const newsList = [];
+newsList.push('<ul>');
 for(let i = 0; i < 10; i++) {
 
-  const div = document.createElement('div');
-  // const li = document.createElement('li');
-  const a = document.createElement('a');
-
-  div.innerHTML = `
-  <li>
-    <a href="#${newsFeed[i].id}">
-      ${newsFeed[i].title} (${newsFeed[i].comments_count})</a>
-  </li>
-   `;
-
-  // a.href = `#${newsFeed[i].id}`
-  // a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
-  // li.appendChild(a);
-
-  ul.appendChild(div.firstElementChild);
+  newsList.push(  
+    `<li>
+      <a href="#${newsFeed[i].id}">${newsFeed[i].title} (${newsFeed[i].comments_count})</a>
+    </li>
+   `);
   
 } 
 
-container.appendChild(ul);
-container.appendChild(content);
+newsList.push('</ul>');  
+container.innerHTML = newsList.join('');
